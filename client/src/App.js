054e9781179cd2b22ed67abdbce53a40f9b1235e
MZeeshanK <malikzeeshankhurshid@gmail.com>
2022-12-18 16:30:24 +0530

    created material ui setup , navbar and light and dark mode feature

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,30 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from 'pages/HomePage';
-import LoginPage from 'pages/LoginPage';
-import ProfilePage from 'pages/ProfilePage';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import ProfilePage from './pages/ProfilePage';
+import Navbar from './components/Navbar';
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import { themeSettings } from './theme';
 
 const App = () => {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div className="app">
       <Router>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/profile/:userId" element={<ProfilePage />} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
       </Router>
     </div>
   );
